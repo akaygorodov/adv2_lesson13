1d5ce888639f10b9989e274e5ea699820b4d6b7b
Rost <rostpanda@gmail.com>
2021-06-11 13:26:44 +0300

    Update Main.java

--- src/main/java/com/alevel/Main.java
+++ src/main/java/com/alevel/Main.java
@@ -2,25 +2,38 @@ package com.alevel;
 
 import com.alevel.model.DriverLicence;
 import com.alevel.model.Person;
-
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.Function;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         final DriverLicence driverLicence = DriverLicence.builder()
-                                                         .categories(new String[]{"A", "B", "C"})
-                                                         .expireDate(Instant.parse("2020-11-30T18:35:24.00Z"))
-                                                         .build();
+                .categories(new String[]{"A", "B", "C"})
+                .expireDate(Instant.parse("1999-11-30T18:35:24.00Z"))
+                .build();
         final Person person = Person.builder()
-                                    .firstName("John")
-                                    .lastName("Smith")
-                                    .driverLicence(Optional.ofNullable(driverLicence))
-                                    .build();
+                .firstName("John")
+                .lastName("Smith")
+                .driverLicence(Optional.ofNullable(driverLicence))
+                .build();
 
-        Optional.ofNullable(person)
-                // you code here
-        ;
+        Instant LicenceTime = Optional.ofNullable(person.getDriverLicence())
+                .flatMap(Function.identity())
+                .map(DriverLicence::getExpireDate)
+                .orElseThrow(() -> new Exception("expireDate input Error"));
+        String[] Categories = Optional.ofNullable(person.getDriverLicence())
+                .flatMap(Function.identity())
+                .map(DriverLicence::getCategories)
+                .orElseThrow(() -> new Exception("categories input Error"));
+
+        if (LicenceTime.isAfter(Instant.now())) {
+            System.out.println(Arrays.toString(Categories));
+
+        }else {
+            throw new Exception("No Active Licence");
+        }
     }
 }
