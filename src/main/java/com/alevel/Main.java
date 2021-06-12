4c525ca0334e6c0935f8d0b4c544dd31f2ae9761
Yaroslav-arch <madivan@protonmail.com>
2021-06-12 22:26:02 +0300

    Added optional person, added LicenseException

--- src/main/java/com/alevel/Main.java
+++ src/main/java/com/alevel/Main.java
@@ -1,26 +1,35 @@
 package com.alevel;
 
+import com.alevel.exceptions.LicenseException;
 import com.alevel.model.DriverLicence;
 import com.alevel.model.Person;
 
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Optional;
 
 public class Main {
 
     public static void main(String[] args) {
         final DriverLicence driverLicence = DriverLicence.builder()
-                                                         .categories(new String[]{"A", "B", "C"})
-                                                         .expireDate(Instant.parse("2020-11-30T18:35:24.00Z"))
-                                                         .build();
+                .categories(new String[]{"A", "B", "C"})
+                .expireDate(Instant.parse("2020-11-30T18:35:24.00Z"))
+                .build();
         final Person person = Person.builder()
-                                    .firstName("John")
-                                    .lastName("Smith")
-                                    .driverLicence(Optional.ofNullable(driverLicence))
-                                    .build();
+                .firstName("John")
+                .lastName("Smith")
+                .driverLicence(Optional.ofNullable(driverLicence))
+                .build();
 
-        Optional.ofNullable(person)
-                // you code here
-        ;
+        Optional.ofNullable(person).ifPresent(p -> {
+            DriverLicence dl = p.getDriverLicence().orElseThrow(LicenseException::new);
+            if (dl.getExpireDate().isBefore(Instant.now())) { // .isBefore() possible NPE
+                throw new LicenseException();
+            }
+            System.out.println("Active categories: " + Arrays.toString(dl.getCategories()));
+
+        });
+
+
     }
 }
