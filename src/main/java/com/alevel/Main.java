5d048e987584aa48ac422f0cd57b218c580771f6
TarasovDmytro <tarasov.personal@gmail.com>
2021-06-13 20:08:20 +0300

    HW13

--- src/main/java/com/alevel/Main.java
+++ src/main/java/com/alevel/Main.java
@@ -6,14 +6,13 @@ import com.alevel.model.Person;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.Optional;
-import java.util.function.Supplier;
 
 public class Main {
 
     public static void main(String[] args) {
         final DriverLicence driverLicence = DriverLicence.builder()
                 .categories(new String[]{"A", "B", "C"})
-                .expireDate(Instant.parse("2021-11-30T18:35:24.00Z"))
+                .expireDate(Instant.parse("2020-11-30T18:35:24.00Z"))
                 .build();
         final Person person = Person.builder()
                 .firstName("John")
@@ -21,14 +20,15 @@ public class Main {
                 .driverLicence(Optional.ofNullable(driverLicence))
                 .build();
 
-        Optional.ofNullable(person).ifPresent(personOptional -> {
-            //noinspection OptionalGetWithoutIsPresent
-            if (Optional.ofNullable(personOptional.getDriverLicence().get().getExpireDate()).isPresent()){
+        Optional.ofNullable(person)
+
+                .ifPresent(personOptional -> {
+                    if (personOptional.getDriverLicence().isPresent()){
                     if (Instant.now().compareTo(personOptional.getDriverLicence().get().getExpireDate()) > 0) {
                         System.out.println("Date of license expired, no valid licenses");
                     } else
                         System.out.println("Active categories: " + Arrays.toString(personOptional.getDriverLicence().get().getCategories()));
-                    }
-                });
+                    }}
+                );
     }
 }
