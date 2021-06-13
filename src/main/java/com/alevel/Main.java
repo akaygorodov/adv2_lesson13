7747a06b365c6ecbac7051c7933364efa7630e63
TarasovDmytro <tarasov.personal@gmail.com>
2021-06-13 17:37:28 +0300

    HW13

--- src/main/java/com/alevel/Main.java
+++ src/main/java/com/alevel/Main.java
@@ -4,23 +4,31 @@ import com.alevel.model.DriverLicence;
 import com.alevel.model.Person;
 
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 public class Main {
 
     public static void main(String[] args) {
         final DriverLicence driverLicence = DriverLicence.builder()
-                                                         .categories(new String[]{"A", "B", "C"})
-                                                         .expireDate(Instant.parse("2020-11-30T18:35:24.00Z"))
-                                                         .build();
+                .categories(new String[]{"A", "B", "C"})
+                .expireDate(Instant.parse("2021-11-30T18:35:24.00Z"))
+                .build();
         final Person person = Person.builder()
-                                    .firstName("John")
-                                    .lastName("Smith")
-                                    .driverLicence(Optional.ofNullable(driverLicence))
-                                    .build();
+                .firstName("John")
+                .lastName("Smith")
+                .driverLicence(Optional.ofNullable(driverLicence))
+                .build();
 
-        Optional.ofNullable(person)
-                // you code here
-        ;
+        Optional.ofNullable(person).ifPresent(personOptional -> {
+            //noinspection OptionalGetWithoutIsPresent
+            if (Optional.ofNullable(person.getDriverLicence().get().getExpireDate()).isPresent()){
+                    if (Instant.now().compareTo(person.getDriverLicence().get().getExpireDate()) > 0) {
+                        System.out.println("Date of license expired, no valid licenses");
+                    } else
+                        System.out.println("Active categories: " + Arrays.toString(person.getDriverLicence().get().getCategories()));
+                    }
+                });
     }
 }
