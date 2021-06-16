5e627f3e9466f47b0b5b5d9066c5bd82c0537e4f
TarasovDmytro <tarasov.personal@gmail.com>
2021-06-16 19:07:56 +0300

    HW13 - refactoring

--- src/main/java/com/alevel/Main.java
+++ src/main/java/com/alevel/Main.java
@@ -2,34 +2,36 @@ package com.alevel;
 
 import com.alevel.model.DriverLicence;
 import com.alevel.model.Person;
+import lombok.SneakyThrows;
 
 import java.time.Instant;
-import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Main {
 
+    @SneakyThrows
     public static void main(String[] args) {
+
         final DriverLicence driverLicence = DriverLicence.builder()
                 .categories(new String[]{"A", "B", "C"})
                 .expireDate(Instant.parse("2020-11-30T18:35:24.00Z"))
                 .build();
+
         final Person person = Person.builder()
                 .firstName("John")
                 .lastName("Smith")
                 .driverLicence(Optional.ofNullable(driverLicence))
                 .build();
 
-        Optional.ofNullable(person)
-
-                .ifPresent(personOptional -> {
-                            if (personOptional.getDriverLicence().isPresent()) {
-                                if (Instant.now().compareTo(personOptional.getDriverLicence().get().getExpireDate()) > 0) {
-                                    System.out.println("Date of license expired, no valid licenses");
-                                } else
-                                    System.out.println("Active categories: " + Arrays.toString(personOptional.getDriverLicence().get().getCategories()));
-                            }
-                        }
-                );
+        List<DriverLicence> personOptional = Optional.ofNullable(person)
+                .stream()
+                .flatMap(personOpt -> personOpt.getDriverLicence().stream())
+                .filter(expireDate -> Instant.now().isBefore(expireDate.getExpireDate()))
+                .collect(Collectors.toList());
+        if (personOptional.isEmpty()) {
+            throw new Exception("Date of license expired, no valid licenses");
+        } else System.out.println(personOptional);
     }
 }
